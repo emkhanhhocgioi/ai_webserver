f0e54a08ae473a71eecf3daaf9ace45dc100df09
unknown <hidrabula@gmail.com>
2026-01-07 17:13:57 +0700

    cloudiary config fix

--- utils/cloudiary-utils.js
+++ utils/cloudiary-utils.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 console.log('Cloudinary Config:', {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 // Configure Cloudinary
 cloudinary.config({
